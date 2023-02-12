c66143805f41dc634db105b10f2e893ca111a079
kanno <812137533@qq.com>
2023-02-12 21:46:16 +0800

    feat: support compress public directory

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,6 @@
+import fsp from 'fs/promises'
+import path from 'path'
+
 export function len<T>(source?: T[] | string | Uint8Array) {
   return source.length
 }
@@ -16,4 +19,19 @@ export function slash(path: string) {
   const isExtendedLengthPath = /^\\\\\?\\/.test(path)
   if (isExtendedLengthPath) return path
   return path.replace(/\\/g, '/')
+}
+
+export async function readAll(entry: string) {
+  const final = []
+  const readAllImpl = async (entry: string) =>
+    Promise.all(
+      (await fsp.readdir(entry)).map(async (dir) => {
+        const p = path.join(entry, dir)
+        if ((await fsp.stat(p)).isDirectory()) return readAllImpl(p)
+        final.push(p)
+        return p
+      })
+    )
+  await readAllImpl(entry)
+  return final as string[]
 }
