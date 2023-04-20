756a1e5dc5517cb80c759b6b6b2c015462e165b4
kanno <812137533@qq.com>
2023-04-20 14:57:58 +0800

    test: update unit test case

--- __tests__/options.spec.ts
+++ __tests__/options.spec.ts
@@ -10,19 +10,27 @@ import type { ViteCompressionPluginConfigAlgorithm } from 'src/interface'
 const getId = () => Math.random().toString(32).slice(2, 10)
 const sleep = (delay: number) => new Promise((resolve) => setTimeout(resolve, delay))
 
-async function mockBuild<T extends Algorithm = never>(conf: ViteCompressionPluginConfigAlgorithm<T>, dir: string) {
+async function mockBuild<T extends Algorithm = never>(
+  conf: ViteCompressionPluginConfigAlgorithm<T>,
+  dir: string,
+  single = false
+) {
   const id = getId()
   await build({
     build: {
       rollupOptions: {
-        output: [
-          {
-            dir: path.join(__dirname, 'temp', id)
-          },
-          {
-            dir: path.join(__dirname, '.tmpl', id)
-          }
-        ]
+        output: !single
+          ? [
+              {
+                dir: path.join(__dirname, 'temp', id)
+              },
+              {
+                dir: path.join(__dirname, '.tmpl', id)
+              }
+            ]
+          : {
+              dir: path.join(__dirname, '.tmpl', id)
+            }
       }
     },
     root: path.join(__dirname, 'fixtures', dir),
@@ -47,4 +55,12 @@ test('rollupOptions First', async (t) => {
   const gz = r.map((v) => v.filter((s) => s.endsWith('.gz')))
   t.is(gz[0].length, 1)
   t.is(gz[1].length, 1)
+})
+
+test('rollupOptions with single output', async (t) => {
+  const id = await mockBuild({ deleteOriginalAssets: true, include: /\.(html)$/ }, 'dynamic', true)
+  await sleep(3000)
+  const r = readAll(path.join(tmplPath, id))
+  const gz = (await r).filter((v) => v.endsWith('.gz'))
+  t.is(gz.length, 1)
 })
