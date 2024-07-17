2ccdee1804338b07197e3d597ea2f68a509f8125
kanno <812137533@qq.com>
2024-07-17 15:46:33 +0800

    chore: clean example

--- example/client/src/dynamic.js
+++ example/client/src/dynamic.js
@@ -1,3 +1,12 @@
 import('./dynamic.css')
 
 export const d = 'dynamic'
+
+export function injectElement() {
+  const app = document.querySelector('#app')
+
+  const el = document.createElement('p')
+
+  el.textContent = 'Dynamic Element'
+  app.appendChild(el)
+}
